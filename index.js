47fa3f50520d5c83f755cc50d0cda807c70668bf
Matheus-fdte <matheus.barbieri@fdte.io>
2020-03-19 19:02:28 -0300

    refact

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const Compose = require('./lib/compose');
-const Core = require('./lib/core');
+const compose = require('./lib/compose');
+const Core = require('./lib/models/core');
 
-const compose = {
-    Compose: Compose,
+
+module.exports = {
+    compose: compose,
     Core: Core
 };
-
-module.exports = compose;
